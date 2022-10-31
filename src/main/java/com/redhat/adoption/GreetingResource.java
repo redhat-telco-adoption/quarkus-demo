3c25a1d590eb503a031d867c2e970b89d6d21332
Jose Mayorga <jmayorga@redhat.com>
2022-10-31 11:59:26 -0500

    Update using environment variables

--- src/main/java/com/redhat/adoption/GreetingResource.java
+++ src/main/java/com/redhat/adoption/GreetingResource.java
@@ -1,16 +1,29 @@
 package com.redhat.adoption;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "kubernetes.namespace", defaultValue = "Not defined!")
+    String k8sNamespace;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> hello() {
+
+        Map<String, String> resp = new HashMap<>();
+
+        resp.put("msg", "Quarkus Demo Application");
+        resp.put("namespace", k8sNamespace);
+
+        return resp;
     }
 }
--- src/test/java/com/redhat/adoption/GreetingResourceTest.java
+++ src/test/java/com/redhat/adoption/GreetingResourceTest.java
@@ -3,8 +3,11 @@ package com.redhat.adoption;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -15,7 +18,7 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .body(is(notNullValue()));
     }
 
 }
